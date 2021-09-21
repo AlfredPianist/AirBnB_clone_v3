0a16c9b734e7ed403c0b385403e4fac01c4a427d
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-20 23:24:15 -0500

    Add: documentation for cities and partially places

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,7 +11,73 @@ from models import storage
 @app_views.route('/states/<state_id>/cities',
                  strict_slashes=False, methods=['GET'])
 def get_all_cities_from_state(state_id):
-    """Returns all city objects related to a state object"""
+    """
+    Get all cities from a state
+    ---
+    tags:
+      - Cities
+    parameters:
+      - name: state_id
+        description: State's id
+        in: path
+        type: string
+        required: true
+        example: d2398800-dd87-482b-be21-50a3063858ad
+    responses:
+      404:
+        description: No state found
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Not found"
+              example: "Not found"
+      200:
+        description: All cities from a given state
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              __class__:
+                type: string
+                description: The object's class
+              created_at:
+                type: string
+                description: City creation date (YY-mm-ddTHH:mm.ffffff)
+              id:
+                type: string
+                description: City's uuid4
+              name:
+                type: string
+                description: City's name
+              state_id:
+                type: string
+                description: State's id
+              updated_at:
+                type: string
+                description: City update date (YY-mm-ddTHH:mm.ffffff)
+          example:
+            [
+              {
+                "__class__": "City",
+                "created_at": "2017-03-25T02:17:06.000000",
+                "id": "1da255c0-f023-4779-8134-2b1b40f87683",
+                "name": "New Orleans",
+                "state_id": "2b9a4627-8a9e-4f32-a752-9a84fa7f4efd",
+                "updated_at": "2017-03-25T02:17:06.000000"
+              },
+              {
+                "__class__": "City",
+                "created_at": "2017-03-25T02:17:06.000000",
+                "id": "45903748-fa39-4cd0-8a0b-c62bfe471702",
+                "name": "Lafayette",
+                "state_id": "2b9a4627-8a9e-4f32-a752-9a84fa7f4efd",
+                "updated_at": "2017-03-25T02:17:06.000000"
+              }
+            ]
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
@@ -22,7 +88,55 @@ def get_all_cities_from_state(state_id):
 @app_views.route('/cities/<city_id>',
                  strict_slashes=False, methods=['GET'])
 def get_city_by_id(city_id):
-    """Returns a city object based on its id. Error if not found"""
+    """
+    Get a city based on its ID
+    ---
+    tags:
+      - Cities
+    parameters:
+      - name: city_id
+        description: City's id
+        in: path
+        type: string
+        required: true
+        example: 5481bd82-04ab-4a58-ae01-d67443aec20c
+    responses:
+      404:
+        description: No city found
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Not found"
+              example: "Not found"
+      200:
+        description: City found
+        schema:
+          type: object
+          properties:
+            __class__:
+              type: string
+              description: The object's class
+            created_at:
+              type: string
+              description: City creation date (YY-mm-ddTHH:mm.ffffff)
+            id:
+              type: string
+              description: City's uuid4
+            name:
+              type: string
+              description: City's name
+            updated_at:
+              type: string
+              description: City update date (YY-mm-ddTHH:mm.ffffff)
+          example:
+            __class__: "City"
+            created_at: "2017-03-25T02:17:06.000000"
+            id: "5481bd82-04ab-4a58-ae01-d67443aec20c"
+            name: "Denver"
+            updated_at: "2017-03-25T02:17:06.000000"
+    """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
@@ -32,7 +146,64 @@ def get_city_by_id(city_id):
 @app_views.route('/states/<state_id>/cities',
                  strict_slashes=False, methods=['POST'])
 def create_city(state_id):
-    """Stores and returns a new city in a given state"""
+    """
+    Create a new city in a given state
+    ---
+    tags:
+      - Cities
+    parameters:
+      - name: create_body
+        description: City's information to be stored
+        in: body
+        type: application/json
+        required: true
+        schema:
+          type: object
+          required:
+            - name
+          properties:
+            name:
+              type: string
+              description: City's name
+          example:
+            name: "New York City"
+    responses:
+      400:
+        description: User error
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Missing name"
+              example: "Missing name"
+      201:
+        description: City created
+        schema:
+          type: object
+          properties:
+            __class__:
+              type: string
+              description: The object's class
+            created_at:
+              type: string
+              description: City creation date (YY-mm-ddTHH:mm.ffffff)
+            id:
+              type: string
+              description: City's uuid4
+            name:
+              type: string
+              description: City's name
+            updated_at:
+              type: string
+              description: City update date (YY-mm-ddTHH:mm.ffffff)
+          example:
+            __class__: "City"
+            created_at: "2017-03-25T02:17:06.000000"
+            id: "6149e15b-90a4-4d42-9d00-8342774d18b6"
+            name: "New York City"
+            updated_at: "2017-03-25T02:17:06.000000"
+    """
     city_json = request.get_json(silent=True)
     if not city_json:
         return jsonify({'error': 'Not a JSON'}), 400
@@ -50,7 +221,73 @@ def create_city(state_id):
 @app_views.route('/cities/<city_id>',
                  strict_slashes=False, methods=['PUT'])
 def update_city(city_id):
-    """Updates and returns the information of a given city"""
+    """
+    Update a city based on its ID
+    ---
+    tags:
+      - Cities
+    parameters:
+      - name: city_id
+        description: City's id
+        in: path
+        type: string
+        required: true
+        example: 6149e15b-90a4-4d42-9d00-8342774d18b6
+      - name: update_body
+        description: City's information to be updated
+        in: body
+        type: application/json
+        required: true
+        example:
+          {
+            "name": "Mountain View"
+          }
+    responses:
+      400:
+        description: Invalid JSON
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Not a JSON"
+              example: "Not a JSON"
+      404:
+        description: No city found
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Not found"
+              example: "Not found"
+      200:
+        description: City updated
+        schema:
+          type: object
+          properties:
+            __class__:
+              type: string
+              description: The object's class
+            created_at:
+              type: string
+              description: City creation date (YY-mm-ddTHH:mm.ffffff)
+            id:
+              type: string
+              description: City's uuid4
+            name:
+              type: string
+              description: City's name
+            updated_at:
+              type: string
+              description: City update date (YY-mm-ddTHH:mm.ffffff)
+          example:
+            __class__: "City"
+            created_at: "2017-03-25T02:17:06.000000"
+            id: "6149e15b-90a4-4d42-9d00-8342774d18b6"
+            name: "Mountain View"
+            updated_at: "2017-03-25T02:17:06.000000"
+    """
     city_json = request.get_json(silent=True)
     if not city_json:
         return jsonify({'error': 'Not a JSON'}), 400
@@ -67,7 +304,34 @@ def update_city(city_id):
 @app_views.route('/cities/<city_id>',
                  strict_slashes=False, methods=['DELETE'])
 def delete_city(city_id):
-    """Deletes a city and returns an empty JSON"""
+    """
+    Delete a city based on its ID
+    ---
+    tags:
+      - Cities
+    parameters:
+      - name: city_id
+        description: City's id
+        in: path
+        type: string
+        required: true
+        example: 6149e15b-90a4-4d42-9d00-8342774d18b6
+    responses:
+      404:
+        description: No city found
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Not found"
+              example: "Not found"
+      200:
+        description: City deleted
+        schema:
+          type: object
+          properties:
+    """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -12,7 +12,110 @@ from models import storage
 @app_views.route('/cities/<city_id>/places',
                  strict_slashes=False, methods=['GET'])
 def get_all_places_from_city(city_id):
-    """Returns all place objects related to a city object"""
+    """
+    Get all places from a city
+    ---
+    tags:
+      - Places
+    parameters:
+      - name: city_id
+        description: City's id
+        in: path
+        type: string
+        required: true
+        example: 1da255c0-f023-4779-8134-2b1b40f87683
+    responses:
+      404:
+        description: No city found
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Not found"
+              example: "Not found"
+      200:
+        description: All places from a given city
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              __class__:
+                type: string
+                description: The object's class
+              city_id:
+                type: string
+                description: City's id
+              created_at:
+                type: string
+                description: Place creation date (YY-mm-ddTHH:mm.ffffff)
+              description:
+                type: string
+                description: Place's description
+              id:
+                type: string
+                description: Place's uuid4
+              latitude:
+                type: number
+                description: Place's latitude
+              longitude:
+                type: number
+                description: Place's longitude
+              max_guest:
+                type: number
+                description: Place's maximum number of guests allowed
+              name:
+                type: string
+                description: Place's name
+              number_bathrooms:
+                type: number
+                description: Place's number of bathrooms
+              number_rooms:
+                type: number
+                description: Place's number of rooms
+              updated_at:
+                type: string
+                description: Place update date (YY-mm-ddTHH:mm.ffffff)
+              user_id:
+                type: string
+                description: User's id
+          example:
+            [
+              {
+                "__class__": "Place",
+                "city_id": "1da255c0-f023-4779-8134-2b1b40f87683",
+                "created_at": "2017-03-25T02:17:06.000000",
+                "description": "The guest house  [...]",
+                "id": "279b355e-ff9a-4b85-8114-6db7ad2a4cd2",
+                "latitude": 29.9493,
+                "longitude": -90.1171,
+                "max_guest": 2,
+                "name": "Guest House by Tulane",
+                "number_bathrooms": 1,
+                "number_rooms": 0,
+                "price_by_night": 60,
+                "updated_at": "2017-03-25T02:17:06.000000",
+                "user_id": "8394fd35-8a8a-479f-a398-48f53b4a6554"
+              },
+              {
+                "__class__": "Place",
+                "city_id": "1da255c0-f023-4779-8134-2b1b40f87683",
+                "created_at": "2017-03-25T02:17:06.000000",
+                "description": "Semi-private room [...]",
+                "id": "ffcc9c22-759e-4418-b788-81eda89c2865",
+                "latitude": 29.9666,
+                "longitude": -90.0519,
+                "max_guest": 1,
+                "name": "Affordable room in the Marigny",
+                "number_bathrooms": 1,
+                "number_rooms": 1,
+                "price_by_night": 40,
+                "updated_at": "2017-03-25T02:17:06.000000",
+                "user_id": "7771bbe9-92ab-46d1-a636-864526361d7d"
+              }
+            ]
+    """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
@@ -23,7 +126,88 @@ def get_all_places_from_city(city_id):
 @app_views.route('/places/<place_id>',
                  strict_slashes=False, methods=['GET'])
 def get_place_by_id(place_id):
-    """Returns a place object based on its id. Error if not found"""
+    """
+    Get a place based on its ID
+    ---
+    tags:
+      - Places
+    parameters:
+      - name: place_id
+        description: Place's id
+        in: path
+        type: string
+        required: true
+        example: 5481bd82-04ab-4a58-ae01-d67443aec20c
+    responses:
+      404:
+        description: No place found
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Not found"
+              example: "Not found"
+      200:
+        description: Place found
+        schema:
+          type: object
+          properties:
+            __class__:
+              type: string
+              description: The object's class
+            city_id:
+              type: string
+              description: City's id
+            created_at:
+              type: string
+              description: Place creation date (YY-mm-ddTHH:mm.ffffff)
+            description:
+              type: string
+              description: Place's description
+            id:
+              type: string
+              description: Place's uuid4
+            latitude:
+              type: number
+              description: Place's latitude
+            longitude:
+              type: number
+              description: Place's longitude
+            max_guest:
+              type: number
+              description: Place's maximum number of guests allowed
+            name:
+              type: string
+              description: Place's name
+            number_bathrooms:
+              type: number
+              description: Place's number of bathrooms
+            number_rooms:
+              type: number
+              description: Place's number of rooms
+            updated_at:
+              type: string
+              description: Place update date (YY-mm-ddTHH:mm.ffffff)
+            user_id:
+              type: string
+              description: User's id
+          example:
+            __class__: "Place"
+            city_id: "1da255c0-f023-4779-8134-2b1b40f87683"
+            created_at: "2017-03-25T02:17:06.000000"
+            description: "The guest house  [...]"
+            id: "279b355e-ff9a-4b85-8114-6db7ad2a4cd2"
+            latitude: 29.9493
+            longitude: -90.1171
+            max_guest: 2
+            name: "Guest House by Tulane"
+            number_bathrooms: 1
+            number_rooms: 0
+            price_by_night: 60
+            updated_at: "2017-03-25T02:17:06.000000"
+            user_id: "8394fd35-8a8a-479f-a398-48f53b4a6554"
+    """
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -33,7 +217,102 @@ def get_place_by_id(place_id):
 @app_views.route('/cities/<city_id>/places',
                  strict_slashes=False, methods=['POST'])
 def create_place(city_id):
-    """Stores and returns a new place in a given city"""
+    """
+    Create a new place in a given city
+    ---
+    tags:
+      - Places
+    parameters:
+      - name: create_body
+        description: Place's information to be stored
+        in: body
+        type: application/json
+        required: true
+        schema:
+          type: object
+          required:
+            - name
+            - user_id
+          properties:
+            name:
+              type: string
+              description: City's name
+            user_id:
+              type: string
+              description: User's id
+          example:
+            name: "Oh... So Sweet Home!! By UNIVERSAL!! (CAPTAIN A.)"
+            user_id: "61302be9-4b31-4be0-92fc-d0dda253e167"
+    responses:
+      400:
+        description: User error
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              default: "Missing name"
+              example: "Missing name"
+      201:
+        description: Place created
+        schema:
+          type: object
+          properties:
+            __class__:
+              type: string
+              description: The object's class
+            city_id:
+              type: string
+              description: City's id
+            created_at:
+              type: string
+              description: Place creation date (YY-mm-ddTHH:mm.ffffff)
+            description:
+              type: string
+              description: Place's description
+            id:
+              type: string
+              description: Place's uuid4
+            latitude:
+              type: number
+              description: Place's latitude
+            longitude:
+              type: number
+              description: Place's longitude
+            max_guest:
+              type: number
+              description: Place's maximum number of guests allowed
+            name:
+              type: string
+              description: Place's name
+            number_bathrooms:
+              type: number
+              description: Place's number of bathrooms
+            number_rooms:
+              type: number
+              description: Place's number of rooms
+            updated_at:
+              type: string
+              description: Place update date (YY-mm-ddTHH:mm.ffffff)
+            user_id:
+              type: string
+              description: User's id
+          example:
+            __class__: "Place"
+            city_id: "712ffb97-b0eb-42f9-8cb9-69548882ab5d"
+            created_at: "2017-03-25T02:17:06.000000"
+            description: "CAPTAIN AMERICA ROOM<BR /><BR /> [...]"
+            id: "38e38612-a626-47a9-a699-05efa178e155"
+            latitude: 28.5558
+            longitude: -81.4697
+            max_guest: 1
+            name: "Oh... So Sweet Home!! By UNIVERSAL!! (CAPTAIN A.)"
+            number_bathrooms: 2
+            number_rooms: 1
+            price_by_night: 16
+            updated_at: "2017-03-25T02:17:06.000000"
+            user_id: "61302be9-4b31-4be0-92fc-d0dda253e167"
+    """
     place_json = request.get_json(silent=True)
     if not place_json:
         return jsonify({'error': 'Not a JSON'}), 400
